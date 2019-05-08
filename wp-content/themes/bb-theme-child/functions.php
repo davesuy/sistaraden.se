74cf97a86ca0db076f214d433d28799b7165bb14
Davesuy <davesuy@gmail.com>
2019-05-08 21:56:02 +0800

    add enque_script and js file

--- wp-content/themes/bb-theme-child/functions.php
+++ wp-content/themes/bb-theme-child/functions.php
@@ -9,3 +9,13 @@ require_once 'classes/class-fl-child-theme.php';
 
 // Actions
 add_action( 'wp_enqueue_scripts', 'FLChildTheme::enqueue_scripts', 1000 );
+
+
+function add_sistaraden_scripts() {
+
+  wp_enqueue_style( 'main', get_stylesheet_directory_uri() . '/css/style.css', array(), '1.1', 'all');
+ 
+  wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/js/custom_script.js', array ( 'jquery' ), 1.1, true);
+ 
+}
+add_action( 'wp_enqueue_scripts', 'add_sistaraden_scripts' );
