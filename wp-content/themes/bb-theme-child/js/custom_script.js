613891b98fb0222407933c36f1fbc72708b79e22
Davesuy <davesuy@gmail.com>
2019-06-07 21:47:11 +0800

    make border fast

--- wp-content/themes/bb-theme-child/js/custom_script.js
+++ wp-content/themes/bb-theme-child/js/custom_script.js
@@ -7,11 +7,11 @@ jQuery(document).ready(function( $ ) {
 	$('.esg-grid .mainul li.eg-sistaraden-wrapper').mouseenter(function(){
 			$(this).find('.eg-sistaraden-element-0-a a').animate({
 				width: "220"
-			},'fast');
+			},0.1);
 		}).mouseleave(function(){
 			$(this).find('.eg-sistaraden-element-0-a a').animate({
 				width: boxWidth + 3
-			}, 'fast');
+			}, 0.1);
 		});
 
 	// Placeholder for subscribe field
