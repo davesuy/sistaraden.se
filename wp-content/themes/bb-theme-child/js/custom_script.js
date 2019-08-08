00ed48f3dabe7605d52aa555ebf61aa94efe63d9
Davesuy <davesuy@gmail.com>
2019-08-08 19:42:22 +0800

    window load cookie

--- wp-content/themes/bb-theme-child/js/custom_script.js
+++ wp-content/themes/bb-theme-child/js/custom_script.js
@@ -406,4 +406,47 @@ jQuery(document).ready(function( $ ) {
 
 	});
 
+
+
+});
+
+
+
+
+function getCookie(name) {
+    var dc = document.cookie;
+    var prefix = name + "=";
+    var begin = dc.indexOf("; " + prefix);
+    if (begin == -1) {
+        begin = dc.indexOf(prefix);
+        if (begin != 0) return null;
+    }
+    else
+    {
+        begin += 2;
+        var end = document.cookie.indexOf(";", begin);
+        if (end == -1) {
+        end = dc.length;
+        }
+    }
+    // because unescape has been deprecated, replaced with decodeURI
+    //return unescape(dc.substring(begin + prefix.length, end));
+    return decodeURI(dc.substring(begin + prefix.length, end));
+} 
+
+
+jQuery(window).load(function( $ ) {
+
+	var cook_user = getCookie('username'); 
+
+	console.log(cook_user);
+
+	if (cook_user == 'newuser') {
+
+         $("#cookie-consent-banner ").hide();
+    }
+    else {
+        // do cookie exists stuff
+    }
+
 });
